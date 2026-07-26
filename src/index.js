cb29168dfb96af500cbb588b44a97198bc94520b
fortuneeta5-png <fortuneeta5@gmail.com>
2026-07-26 19:58:13 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,10 +5,14 @@ export default {
 
     const hasVendorSubdomain = hostParts.length > 2 && hostParts[0] !== 'www';
 
-    if (hasVendorSubdomain) {
+    if (hasVendorSubdomain && (url.pathname === '/' || url.pathname === '')) {
       const rewrittenUrl = new URL(request.url);
       rewrittenUrl.pathname = '/store.html';
-      return env.ASSETS.fetch(new Request(rewrittenUrl, request));
+      const assetRequest = new Request(rewrittenUrl.toString(), {
+        method: request.method,
+        headers: request.headers,
+      });
+      return env.ASSETS.fetch(assetRequest);
     }
 
     return env.ASSETS.fetch(request);
